b2c627ba3b31801e9f9ef72868bd53cbd35fc1f7
xbdm <xbdm@xbdm.fun>
2023-08-22 12:43:15 -0400

    kept source code path only

--- misc/footer.php
+++ misc/footer.php
@@ -2,14 +2,7 @@
   <div class="footer">
     <p id="footerText">
       |
-      <a href="./" target="_blank">Binternet</a>
-      |
       <a href="https://github.com/Ahwxorg/binternet/" target="_blank">Source code</a>
-      |
-      <a href="https://ahwx.org/donate.php" target="_blank">Donate</a>
-      |
-      <a href="https://github.com/Ahwxorg/binternet#legal-notice" target="_blank">Legal notice</a>
-      |
 
 <?php
 if (isset($images)) {
